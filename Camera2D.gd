f4160fc857e7a485cb9bf515404ce5c150aa46c4
Stephen Frerotte <stephen.frerotte@sharppointdata.com>
2018-05-04 14:27:34 -0400

    Frame out level colliders and train setup.

---
+++ Camera2D.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	if Input.is_action_pressed("ui_right"):
+		Camera2D.position.x += 10;
+        # Move right
+	pass
