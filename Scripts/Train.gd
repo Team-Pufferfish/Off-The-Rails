a0d574bf86f2996b5393b7ace626aed4208c183e
Stephen Frerotte <stephen.frerotte@sharppointdata.com>
2018-05-04 20:37:15 -0400

    Update train physics, level controls and game state flow.

---
+++ Scripts/Train.gd
@@ -0,0 +1,28 @@
+extends Node
+
+# class member variables go here, for example:
+export var passengers = 1000;
+export var open_end = 0;
+export var passenger_tick = 1;
+var delta_time = 0;
+var passenger_particle;
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	passenger_particle = find_node("Passengers")
+	set_process(true);
+	pass
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	delta_time += delta
+	if(delta_time >= passenger_tick && open_end != 0):
+		passenger_particle.emitting = false;
+		var tilt = get_global_transform().get_rotation()
+		#print(name + " rot: %s" % tilt);
+		if((tilt  * open_end) > (0.5 ) && passengers >= 0):
+			passenger_particle.emitting = true;
+			passengers -= 1;
+		#	//print(name + " passengers: %s" % passengers);
